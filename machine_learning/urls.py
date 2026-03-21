17fba75b53a53864b2e266cd25a50543a289e1b0
Adish Jain <adishjain9723@gmail.com>
2026-03-21 12:59:09 +0530

    added a new health-check api

--- machine_learning/urls.py
+++ machine_learning/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("health/", views.health)
+    path("health/", views.health),
+    path("health-check/", views.health_check)
 ]
