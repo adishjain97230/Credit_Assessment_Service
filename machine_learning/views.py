b3110657d59699fb3652df01e1d72607af9d7e9a
Adish Jain <adish.jain@myntra.com>
2026-03-21 12:59:09 +0530

    added a new health-check api

--- machine_learning/views.py
+++ machine_learning/views.py
@@ -1,5 +1,7 @@
+import json
 from django.shortcuts import render
 from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_http_methods
 
 # Create your views here.
@@ -7,5 +9,11 @@ from django.views.decorators.http import require_http_methods
 
 @require_http_methods(["GET"])
 def health(request):
-    print(request)
     return JsonResponse({"status": "ok"})
+
+@require_http_methods(["POST"])
+@csrf_exempt
+def health_check(request):
+    data = json.loads(request.body)
+    headers = request.headers
+    return JsonResponse({"status": "ok", "data": data, "headers": dict(headers)}, status=200)
